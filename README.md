95544ec4ec69746a325e06ad5f871c87e5257310
shvrekhan <shvrekhan@gmail.com>
2022-08-31 00:25:47 +0530

    socket error fix

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-CODE SHARE 
---------------------
-A real time code sharing platform using websocket for real time syncronization of the code.
+### Demo
+
+https://code-sync.codersgyan.com/
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,72 +1,82 @@
-import React ,{useState} from 'react'
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
-import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
+const Home = () => {
+    const navigate = useNavigate();
 
-export const Home = () => {
-  const navigate = useNavigate();
-  const [roomId , setRoomId] = useState('');
-  const [username , setUserName] = useState('');
-  const createNewRoom = (e) =>{
-      e.preventDefault();
-      const id = uuidv4();
-      setRoomId(id);
-      toast.success('created a new room');
-      console.log(id);
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
 
-  }
-  const joinRoom = ()=>{
-     if(!roomId || !username){
-      toast.error('ROOM ID or Username is required');
-      return ;}
-
-      // Redirect
-      navigate("./editor/"+roomId,{
-        state:{
-          username,
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
         }
-      });
 
-  }
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
 
-const handelInputEnter = (e)=>{
-   console.log('event',e.code);
-   if(e.code === 'Enter'){
-    joinRoom();
-
-   }
-} 
-
-  return <div className='homePageWrapper'>
-            <div className='formWrapper'>
-              <img src="/coding-svgrepo-com.svg" className='homeFormImage' alt="code logo"/>
-              <h4 className='mainLabel'> Paste invitation Room Id</h4>
-              <div className='inputGroup'>
-                <input type="text" 
-                       className='inputBox' 
-                       placeholder='Room Id'
-                       onChange={(e)=>setRoomId(e.target.value)} 
-                       value={roomId}
-                       onKeyUp={handelInputEnter}
-                       />
-                <input type="text" 
-                       className='inputBox ' 
-                       placeholder='User Name'
-                       onChange={(e)=>setUserName(e.target.value)} 
-                       value={username}
-                       onKeyUp={handelInputEnter}
-                       />
-                <br></br>
-                <button className='btn joinbtn' onClick={joinRoom}>Join</button>
-                <span className='createInfo'> <br></br>
-                  if you dont have an invite then create &nbsp; 
-                  <a onClick={createNewRoom} href='#' className='createNewBtn'> Create Room </a>
-                </span>
-              </div>
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinRoom();
+        }
+    };
+    return (
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <img
+                    className="homePageLogo"
+                    src="/coding-svgrepo-com.svg"
+                    alt="code-sync-logo"
+                />
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="USERNAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                        If you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
+                </div>
             </div>
-          </div>
-}
+                    </div>
+    );
+};
 
 export default Home;
